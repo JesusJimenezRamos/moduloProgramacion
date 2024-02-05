09586f479f03ae76bb316e3cd1107e9210d19fe9
JesusJimenezRamos <159002665+JesusJimenezRamos@users.noreply.github.com>
2024-02-05 10:02:47 +0100

    Casa PruebaGitHub.java

--- PruebaGitHub/src/PruebaGitHub.java
+++ PruebaGitHub/src/PruebaGitHub.java
@@ -4,7 +4,7 @@ public class PruebaGitHub {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		System.out.println("Hola, GitHub!, Commit hecho.");
+		System.out.println("Hola, GitHub!, Commit hecho. Esto esta hecho desde casa.");
 	}
 
 }
